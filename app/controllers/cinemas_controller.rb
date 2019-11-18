a048a03c54f3afd6d6c1618723b35f8fd9dad9da
juliana geller <bablouchka@gmail.com>
2019-11-18 15:31:15 +0100

    booking

--- app/controllers/cinemas_controller.rb
+++ app/controllers/cinemas_controller.rb
@@ -1,9 +1,5 @@
 class CinemasController < ApplicationController
-<<<<<<< HEAD
-    def index
-=======
   def index
->>>>>>> 8c2082c6cbd6898aa219158cc647814a273b3222
     @cinemas = Cinema.all
   end
 
@@ -12,10 +8,6 @@ class CinemasController < ApplicationController
   end
 
 private
-<<<<<<< HEAD
-=======
-
->>>>>>> 8c2082c6cbd6898aa219158cc647814a273b3222
   def cinema_params
     params.require(:cinema).permit(:name, :address)
   end
