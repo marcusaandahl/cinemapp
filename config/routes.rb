04434b8bea828b3a188d5d5d98ec220b1eff9945
marcusaandahl <55247594+marcusaandahl@users.noreply.github.com>
2019-11-18 15:48:56 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,7 @@ Rails.application.routes.draw do
   resources :rooms, only: [:index, :show]
   resources :sessions, only: [:index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   resources :cinemas, only: [:index, :show]
-=======
   resources :movies, only: [:index, :show]
-<<<<<<< HEAD
   resources :bookings
-=======
->>>>>>> 27d6557448b17afed7c845b24bc0892672f76571
->>>>>>> 8c2082c6cbd6898aa219158cc647814a273b3222
 end
