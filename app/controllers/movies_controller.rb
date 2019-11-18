11ce668410a78705a6a239e7645e5e01bf569a40
juliana geller <bablouchka@gmail.com>
2019-11-18 14:54:21 +0100

    movie model, controller, views and routes created

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,14 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+private
+  def movie_params
+    params.require(:movie).permit(:name, :description)
+  end
+end
