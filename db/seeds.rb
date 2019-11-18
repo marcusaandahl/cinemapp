fcf9e0f901516c3727f6d702ce29934113364117
Henderson Pinto <hendersoncode@gmail.com>
2019-11-18 18:55:47 +0100

    seed and faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,164 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Clearing database'
+
+User.destroy_all
+Movie.destroy_all
+Cinema.destroy_all
+Room.destroy_all
+Seat.destroy_all
+Session.destroy_all
+Booking.destroy_all
+
+
+puts 'Creating users'
+
+henderson = User.new(
+ name: "Henderson",
+ last_name: "Pinto",
+ location: Faker::Address.full_address,
+ email: "henderson@mammamia.com",
+ password: "1234567"
+ )
+henderson.save!
+
+rasmus = User.new(
+ name: "Rasmus",
+ last_name: "Melberg",
+ location: Faker::Address.full_address,
+ email: "rasmus@cinemapp.com",
+ password: "1234567"
+ )
+rasmus.save!
+
+
+marcus = User.new(
+ name: "Marcus",
+ last_name: "Aandahl",
+ location: Faker::Address.full_address,
+ email: "marcus@cinemapp.com",
+ password: "1234567"
+ )
+marcus.save!
+
+juliana = User.new(
+ name: "Juliana",
+ last_name: "Geller",
+ location: Faker::Address.full_address,
+ email: "juliana@cinemapp.com",
+ password: "1234567"
+ )
+juliana.save!
+puts 'saving users'
+
+
+
+################### CINEMAS
+
+puts 'Creating cinemas'
+empire = Cinema.new(
+ name: "Empire Cinema",
+ address: "Guldbergsgade 29F, 2200 København"
+)
+empire.save!
+
+park = Cinema.new(
+ name: "Park Bio",
+ address: "Østerbrogade 79, 2100 København"
+)
+park.save!
+
+falconer = Cinema.new(
+ name: "Nordic Film Cinemas Falconer",
+ address: "Sylows Alle 15, 2000 Frederiksberg"
+)
+falconer.save!
+
+
+palads = Cinema.new(
+ name: "Palads Teatret",
+ address: "Axeltorv 9, 1609 København"
+)
+palads.save!
+
+puts 'saving cinemas'
+
+
+################### MOVIES
+
+puts 'Creating movies'
+terminator = Movie.new(
+ name: "Terminator: Dark Fate",
+ description: "Sarah Connor and a hybrid cyborg human must protect a young girl from a newly modified liquid Terminator from the future."
+)
+terminator.save!
+
+joker = Movie.new(
+ name: "Joker",
+ description: "In Gotham City, mentally-troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: 'The Joker'."
+)
+joker.save!
+
+lion_king = Movie.new(
+ name: "The Lion King",
+ description: "After the murder of his father, a young lion prince flees his kingdom only to learn the true meaning of responsibility and bravery."
+)
+lion_king.save!
+
+avengers = Movie.new(
+ name: "Avengers: Endgame",
+ description: "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe."
+)
+avengers.save!
+
+toystory = Movie.new(
+ name: "Toy Story 4",
+ description: "When a new toy called 'Forky' joins Woody and the gang, a road trip alongside old and new friends reveals how big the world can be for a toy."
+)
+toystory.save!
+
+puts 'saving movies'
+
+
+
+
+################### ROOMS
+
+puts 'Creating rooms'
+rooms = []
+cinemas = [empire, park, falconer, palads]
+cinemas.each { |cinema|
+i = 0
+while i <= 4
+  rooms << Room.create(
+ name: "Room #{i}",
+ cinema: cinema,
+)
+i+=1
+end
+}
+
+puts 'saving rooms...'
+
+
+
+
+################### SEATS
+
+puts 'Creating seats'
+rooms.each { |room|
+  rows = (1..15)
+  rows.each {|row|
+    columns = (1..15)
+    columns.each {|column|
+      Seat.create(
+        row: row,
+        column: column,
+        room: room,
+      )
+    }
+  }
+}
+puts "saving seats for room #{self} "
+
