877b1eb7d998f3f690a5b9ebd9842582a900bf0c
juliana geller <bablouchka@gmail.com>
2019-11-19 16:20:14 +0100

    carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,9 @@
 import "bootstrap";
 import "../dynamics/navbar";
+import 'slick-carousel';
+
+import { initCarousel } from "../components/carousel";
+
+console.log('Hello from Webpack!')
+
+initCarousel();
