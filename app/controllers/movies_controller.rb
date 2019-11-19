a10a1b831ae6db2589228a0f1f97ff2aa1ec9755
juliana geller <bablouchka@gmail.com>
2019-11-19 10:13:11 +0100

    search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,13 @@ class MoviesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+   if params[:query].present?
+    sql_query = "name ILIKE :query OR description ILIKE :query"
+      @movies = Movie.where(sql_query, query: "%#{params[:query]}%")
+  else
     @movies = Movie.all
+
+  end
   end
 
   def show
