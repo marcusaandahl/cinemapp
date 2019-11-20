0d388fc13c404f86f9bf04502cca99e866d9146c
Henderson Pinto <hendersoncode@gmail.com>
2019-11-20 13:29:40 +0100

    added a CSS components view /components

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :components]
 
   def home
+  end
+
+  def components
   end
 end
