cae54c3e0ecb1f32fb48f8655dd52f8461c5b547
juliana geller <bablouchka@gmail.com>
2019-11-25 18:43:53 +0100

    cinema index and small fixes

--- app/models/cinema.rb
+++ app/models/cinema.rb
@@ -1,6 +1,7 @@
 class Cinema < ApplicationRecord
   has_many :rooms, dependent: :destroy
-
+  has_many :sessions, through: :rooms
+  has_many :movies, through: :sessions
   validates :name, presence: true
   validates :address, uniqueness: true, presence: true
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   has_many :sessions, dependent: :destroy
+  has_many :rooms, through: :sessions
+  has_many :cinemas, through: :rooms
 
   validates :name, uniqueness: true, presence: true
 end
