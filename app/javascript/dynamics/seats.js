3e9acad8be8fe5d16db1f7e4530ebc2b80ddec1a
juliana geller <bablouchka@gmail.com>
2019-11-25 16:34:27 +0100

    session saving and little adjustments

--- app/javascript/dynamics/seats.js
+++ app/javascript/dynamics/seats.js
@@ -28,7 +28,7 @@ const displayPrice = () => {
   discountedSpan.innerHTML = discountedSum + " kr"
 
   const savingSpan = document.querySelector('#saving-span');
-  savingSpan.innerHTML = "You save " + (baseSum -= discountedSum) + " kr"
+  savingSpan.innerHTML = "• You save " + (baseSum -= discountedSum) + " kr"
 }
 
 
