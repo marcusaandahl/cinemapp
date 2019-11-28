f81b6ffa7c76ad1161fc7a2e5570fcd2fd1dbe80
Marcus Andreas Aandahl <marcus.aandahl@gmail.com>
2019-11-28 13:17:41 +0100

    added see own bookings

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,8 @@ class SessionsController < ApplicationController
     @room = @session.room
     @cinema = @room.cinema
     @seats = @room.seats
+    @user_seats = getUserSeats(@session)
+    puts @user_seats
   end
 
   # def showtime
@@ -22,5 +24,17 @@ class SessionsController < ApplicationController
 
   def strong_params
     params.require(:session).permit(:showtime)
+  end
+
+  def getUserSeats(session)
+    userSeats = []
+    current_user.orders.each do |order|
+      if order.bookings.first.session_id == session.id
+        order.bookings.each do |booking|
+          userSeats.push(booking.seat_id)
+        end
+      end
+    end
+    return userSeats
   end
 end
